790d1903d5b9c00db4cb7db759f339a4f2eb5686
Rob Kreisel <rob@deisgn1010.com>
2023-01-14 22:18:42 -0600

    Users api resource with crud

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create(
+            $request->only('name', 'email')
+            + ['password' => Hash::make(1234)]
+        );
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $user->update($request->only('name', 'email'));
+
+        return response($user, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +26,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('users', UserController::class);
 });
