2fbbc0c0091cce589d39afc6e06c05f5e3a47a73
Rob Kreisel <rob@deisgn1010.com>
2023-01-14 20:23:42 -0600

    Register a user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+}
