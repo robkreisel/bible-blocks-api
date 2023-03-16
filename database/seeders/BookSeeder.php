66f87a4c0968d029a7cc84eee4d547cfe7ea8b48
Rob Kreisel <rob@deisgn1010.com>
2023-03-15 22:13:05 -0500

    Adding more

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -24,7 +24,6 @@ class BookSeeder extends Seeder
             'author' => 'Moses',
             'order' => 1
         ]);
-
         Book::factory()->create([
             'name' => 'Exodus',
             'symbol' => 'Ex',
@@ -34,7 +33,6 @@ class BookSeeder extends Seeder
             'author' => 'Moses',
             'order' => 1
         ]);
-
         Book::factory()->create([
             'name' => 'Leviticus',
             'symbol' => 'Lv',
@@ -44,7 +42,6 @@ class BookSeeder extends Seeder
             'author' => 'Moses',
             'order' => 1
         ]);
-
         Book::factory()->create([
             'name' => 'Numbers',
             'symbol' => 'Nu',
@@ -54,7 +51,6 @@ class BookSeeder extends Seeder
             'author' => 'Moses',
             'order' => 1
         ]);
-
         Book::factory()->create([
             'name' => 'Deuteronomy',
             'symbol' => 'Dt',
@@ -63,6 +59,312 @@ class BookSeeder extends Seeder
             'era' => 'exodus',
             'author' => 'Moses',
             'order' => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Joshua",
+            "symbol" => "Js",
+            "category" => "history",
+            "year" => "1375 BC",
+            "era" => "conquest",
+            "author" => "Joshua",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Judges",
+            "symbol" => "Ju",
+            "category" => "history",
+            "year" => "1050 BC",
+            "era" => "judges",
+            "author" => "Samuel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Ruth",
+            "symbol" => "Ru",
+            "category" => "history",
+            "year" => "1050-1000 BC",
+            "era" => "judges",
+            "author" => "Samuel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "1 Samuel",
+            "symbol" => "1Sa",
+            "category" => "history",
+            "year" => "900 BC",
+            "era" => "kingdom",
+            "author" => "Samuel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "2 Samuel",
+            "symbol" => "2Sa",
+            "category" => "history",
+            "year" => "900 BC",
+            "era" => "kingdom",
+            "author" => "Samuel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "1 Kings",
+            "symbol" => "1Ki",
+            "category" => "history",
+            "year" => "550 BC",
+            "era" => "kingdom",
+            "author" => "Unknown",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "2 Kings",
+            "symbol" => "2Ki",
+            "category" => "history",
+            "year" => "550 BC",
+            "era" => "kingdom",
+            "author" => "Unknown",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "1 Chronicles",
+            "symbol" => "1Ch",
+            "category" => "history",
+            "year" => "450 BC",
+            "era" => "kingdom",
+            "author" => "Unknown",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "2 Chronicles",
+            "symbol" => "2Ch",
+            "category" => "history",
+            "year" => "340 BC",
+            "era" => "kingdom",
+            "author" => "Unknown",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Ezra",
+            "symbol" => "Er",
+            "category" => "history",
+            "year" => "450 BC",
+            "era" => "return",
+            "author" => "Ezra",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Nehemiah",
+            "symbol" => "Ne",
+            "category" => "history",
+            "year" => "425-400 BC",
+            "era" => "return",
+            "author" => "Nehemiah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Esther",
+            "symbol" => "Es",
+            "category" => "history",
+            "year" => "450 BC",
+            "era" => "return",
+            "author" => "Unknown",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Job",
+            "symbol" => "Jb",
+            "category" => "writing",
+            "year" => "1900 BC",
+            "era" => "patriarch",
+            "author" => "Unknown",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Psalms",
+            "symbol" => "Ps",
+            "category" => "writing",
+            "year" => "1400-450 BC",
+            "era" => "kingdom",
+            "author" => "Various",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Proverbs",
+            "symbol" => "Pr",
+            "category" => "writing",
+            "year" => "970-675 BC",
+            "era" => "kingdom",
+            "author" => "Solomon",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Ecclesiastes",
+            "symbol" => "Ec",
+            "category" => "writing",
+            "year" => "940-931 BC",
+            "era" => "kingdom",
+            "author" => "Solomon",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Song of Songs",
+            "symbol" => "Ss",
+            "category" => "writing",
+            "year" => "970-950 BC",
+            "era" => "kingdom",
+            "author" => "Solomon",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Isaiah",
+            "symbol" => "Is",
+            "category" => "prophecy",
+            "year" => "700-680 BC",
+            "era" => "kingdom",
+            "author" => "Isaiah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Jeremiah",
+            "symbol" => "Je",
+            "category" => "prophecy",
+            "year" => "585-570 BC",
+            "era" => "exile",
+            "author" => "Jeremiah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Lamentations",
+            "symbol" => "La",
+            "category" => "prophecy",
+            "year" => "585 BC",
+            "era" => "exile",
+            "author" => "Jeremiah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Ezekiel",
+            "symbol" => "Ez",
+            "category" => "prophecy",
+            "year" => "590-570 BC",
+            "era" => "exile",
+            "author" => "Ezekiel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Daniel",
+            "symbol" => "Da",
+            "category" => "prophecy",
+            "year" => "536-530 BC",
+            "era" => "exile",
+            "author" => "Daniel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Hosea",
+            "symbol" => "Ho",
+            "category" => "prophecy",
+            "year" => "750-710 BC",
+            "era" => "kingdom",
+            "author" => "Hosea",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Joel",
+            "symbol" => "Jo",
+            "category" => "prophecy",
+            "year" => "Unknown",
+            "era" => "kingdom",
+            "author" => "Joel",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Amos",
+            "symbol" => "Am",
+            "category" => "prophecy",
+            "year" => "750 BC",
+            "era" => "kingdom",
+            "author" => "Amos",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Obadiah",
+            "symbol" => "Ob",
+            "category" => "prophecy",
+            "year" => "Unknown",
+            "era" => "kingdom",
+            "author" => "Obadiah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Jonah",
+            "symbol" => "Jh",
+            "category" => "prophecy",
+            "year" => "772-754 BC",
+            "era" => "kingdom",
+            "author" => "Jonah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Micah",
+            "symbol" => "Mi",
+            "category" => "prophecy",
+            "year" => "735-710 BC",
+            "era" => "kingdom",
+            "author" => "Micah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Nahum",
+            "symbol" => "Na",
+            "category" => "prophecy",
+            "year" => "650 BC",
+            "era" => "kingdom",
+            "author" => "Nahum",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Habbakuk",
+            "symbol" => "Ha",
+            "category" => "prophecy",
+            "year" => "640 BC",
+            "era" => "kingdom",
+            "author" => "Habbakuk",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Zephaniah",
+            "symbol" => "Zp",
+            "category" => "prophecy",
+            "year" => "635-625 BC",
+            "era" => "kingdom",
+            "author" => "Zephaniah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Haggai",
+            "symbol" => "Hg",
+            "category" => "prophecy",
+            "year" => "526 BC",
+            "era" => "kingdom",
+            "author" => "Haggai",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Zechariah",
+            "symbol" => "Zc",
+            "category" => "prophecy",
+            "year" => "520-484 BC",
+            "era" => "kingdom",
+            "author" => "Zechariah",
+            "order" => 1
+        ]);
+        Book::factory()->create([
+            "name" => "Malachi",
+            "symbol" => "Ma",
+            "category" => "prophecy",
+            "year" => "433-424 BC",
+            "era" => "kingdom",
+            "author" => "Malachi",
+            "order" => 1
         ]);
     }
 }
--- database/seeders/ChapterSeeder.php
+++ database/seeders/ChapterSeeder.php
@@ -16,39 +16,58 @@ class ChapterSeeder extends Seeder
      */
     public function run()
     {
-        // Genesis
         $genesis = [31, 25, 24, 26, 32, 22, 24, 22, 29, 32, 32, 20, 18, 24, 21, 16, 27, 33, 38, 18, 34, 24, 20, 67, 34,
             35, 46, 22, 35, 43, 55, 32, 20, 31, 29, 43, 36, 30, 23, 23, 57, 38, 34, 34, 28, 34, 31, 22, 33, 26];
-        for ($i = 1; $i <= count($genesis); $i++) {
-            Chapter::factory()->create(['book_id' => 1, 'chapter' => $i, 'number_verses' => $genesis[$i - 1]]);
-        }
 
-        // Exodus
         $exodus = [22, 25, 22, 31, 23, 30, 25, 32, 35, 29, 10, 51, 22, 31, 27, 36, 16, 27, 25, 26, 36, 31, 33, 18, 40,
             37, 21, 43, 46, 38, 18, 35, 23, 35, 35, 38, 29, 31, 43, 38];
-        for ($i = 1; $i <= count($exodus); $i++) {
-            Chapter::factory()->create(['book_id' => 2, 'chapter' => $i, 'number_verses' => $exodus[$i - 1]]);
-        }
 
-        // Leviticus
         $leviticus = [17, 16, 17, 35, 19, 30, 38, 36, 24, 20, 47, 8, 59, 57, 33, 34, 16, 30, 37, 27, 24, 33, 44, 23, 55,
             46, 34];
-        for ($i = 1; $i <= count($leviticus); $i++) {
-            Chapter::factory()->create(['book_id' => 3, 'chapter' => $i, 'number_verses' => $leviticus[$i - 1]]);
-        }
 
-        // Numbers
         $numbers = [54, 34, 51, 49, 31, 27, 89, 26, 23, 36, 35, 16, 33, 45, 41, 50, 13, 32, 22, 29, 35, 41, 30, 25, 18,
             65, 23, 31, 40, 16, 54, 42, 56, 29, 34, 13];
-        for ($i = 1; $i <= count($numbers); $i++) {
-            Chapter::factory()->create(['book_id' => 4, 'chapter' => $i, 'number_verses' => $numbers[$i - 1]]);
-        }
 
-        // Deuteronomy
         $deuteronomy = [46, 37, 29, 49, 33, 25, 26, 20, 29, 22, 32, 32, 18, 29, 23, 22, 20, 22, 21, 20, 23, 30, 25, 22,
             19, 19, 26, 68, 29, 20, 30, 52, 29, 12];
-        for ($i = 1; $i <= count($deuteronomy); $i++) {
-            Chapter::factory()->create(['book_id' => 5, 'chapter' => $i, 'number_verses' => $deuteronomy[$i - 1]]);
+
+        $joshua = [18, 24, 17, 24, 15, 27, 26, 35, 27, 43, 23, 24, 33, 15, 63, 10, 18, 28, 51, 9, 45, 34, 16, 33];
+
+        $judges = [36, 23, 31, 24, 31, 40, 25, 35, 57, 18, 40, 15, 25, 20, 20, 31, 13, 31, 30, 48, 25];
+
+        $ruth = [22, 23, 18, 22];
+
+        $samuel1 = [28, 36, 21, 22, 12, 21, 17, 22, 27, 27, 15, 25, 23, 52, 35, 23, 58, 30, 24, 42, 15, 23, 29, 22, 44,
+            25, 12, 25, 11, 31, 13];
+
+        $samuel2 = [27, 32, 39, 12, 25, 23, 29, 18, 13, 19, 27, 31, 39, 33, 37, 23, 29, 33, 43, 26, 22, 51, 39, 25];
+
+        $kings1 = [53, 46, 28, 34, 18, 38, 51, 66, 28, 29, 43, 33, 34, 31, 34, 34, 24, 46, 21, 43, 29, 53];
+
+        $kings2 = [18, 25, 27, 44, 27, 33, 20, 29, 37, 36, 21, 21, 25, 29, 38, 20, 41, 37, 37, 21, 26, 20, 37, 20, 30];
+
+        $this->createChaptersForBook($genesis, 1);
+        $this->createChaptersForBook($exodus, 2);
+        $this->createChaptersForBook($leviticus, 3);
+        $this->createChaptersForBook($numbers, 4);
+        $this->createChaptersForBook($deuteronomy, 5);
+        $this->createChaptersForBook($joshua, 6);
+        $this->createChaptersForBook($judges, 7);
+        $this->createChaptersForBook($ruth, 8);
+        $this->createChaptersForBook($samuel1, 9);
+        $this->createChaptersForBook($samuel2, 10);
+        $this->createChaptersForBook($kings1, 11);
+        $this->createChaptersForBook($kings2, 12);
+    }
+
+    private function createChaptersForBook($versesArray, $bookId)
+    {
+        for ($chapter = 1; $chapter <= count($versesArray); $chapter++) {
+            Chapter::factory()->create([
+                'book_id' => $bookId,
+                'chapter' => $chapter,
+                'number_verses' => $versesArray[$chapter - 1]
+            ]);
         }
     }
 }
